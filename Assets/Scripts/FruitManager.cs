0addc4f10a77fe89b873e6d5e1511de133c89a15
DaiSienra <dai.sienra@gmail.com>
2025-06-22 23:52:46 -0300

    conteo de frutas,enemigos,actualizaciones de niveles

--- Assets/Scripts/FruitManager.cs
+++ Assets/Scripts/FruitManager.cs
@@ -2,6 +2,8 @@ using UnityEngine;
 using UnityEngine.SceneManagement; //para cambiar de escena
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.UI;
+using TMPro;
 
 
 
@@ -10,10 +12,33 @@ public class FruitManager : MonoBehaviour
     public int totalFruits;  // Total frutas en la escena
     private int fruitsCollected = 0;
 
+    public TextMeshProUGUI totalFruits2;
+
+    public TextMeshProUGUI fruitCollected;
+
+    private int totalFruitsInLevel;
+
     private void Start()
     {
         totalFruits = transform.childCount; // si las frutas están como hijos del manager
-        // Si no están como hijos, asigna manualmente totalFruits desde el inspector o desde otro método
+                                            // Si no están como hijos, asigna manualmente totalFruits desde el inspector o desde otro método
+        totalFruitsInLevel = transform.childCount;
+    }
+
+    public void Update()
+    {
+            if (totalFruits2 != null)
+        {
+            totalFruits2.text = totalFruitsInLevel.ToString();
+        }
+
+        if (fruitCollected != null)
+        {
+            fruitCollected.text = transform.childCount.ToString();
+        }
+        /*totalFruits2.text = totalFruitsInLevel.ToString();
+        fruitCollected.text = transform.childCount.ToString();
+        */
     }
 
     public void FruitCollected()
