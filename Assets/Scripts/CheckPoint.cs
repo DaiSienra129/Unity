54ef73e9e3262cf937358fcc549135757008556b
DaiSienra <dai.sienra@gmail.com>
2025-06-26 17:07:57 -0300

    ultimos cambios

--- Assets/Scripts/CheckPoint.cs
+++ Assets/Scripts/CheckPoint.cs
@@ -9,7 +9,7 @@ public class CheckPoint : MonoBehaviour
 
     void Start()
     {
-        LevelCleard.text = "";
+        LevelCleard.text = " ";
     }
 
     void OnTriggerEnter2D(Collider2D collision)
@@ -31,5 +31,13 @@ public class CheckPoint : MonoBehaviour
     {
         yield return new WaitForSeconds(2f);
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        // Reiniciar datos de progreso si querés empezar desde cero
+        PlayerPrefs.DeleteKey("checkPointPositionX");
+        PlayerPrefs.DeleteKey("checkPointPositionY");
+        PlayerPrefs.DeleteKey("playerLives");
+        PlayerPrefs.DeleteKey("nivelAlcanzado");
+
+        // Cargar el primer nivel
+        SceneManager.LoadScene(0); // o "Level 1" si preferís por nombre
     }
 }
